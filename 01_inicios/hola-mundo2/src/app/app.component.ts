4863f002ea81aa900253902fcba58114a6f3ca50
kamaxo <kamaxo@gmail.com>
2017-10-16 17:00:57 +0200

    Creacion proyecto hola-mundo2

---
+++ 01_inicios/hola-mundo2/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  curso = 'Angular 4.x';
+  formador = 'Antonio José Camacho Fuentes';
+  empresa = 'Indra Software Labs - Málaga';
+  fecha = '2017';
+}
