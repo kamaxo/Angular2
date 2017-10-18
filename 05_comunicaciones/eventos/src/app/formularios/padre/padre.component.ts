ca2ef7910554af85384a98950f643764fa3b3385
kamaxo <kamaxo@gmail.com>
2017-10-18 16:16:44 +0200

    eventos

---
+++ 05_comunicaciones/eventos/src/app/formularios/padre/padre.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-padre',
+  templateUrl: './padre.component.html',
+  styleUrls: ['./padre.component.css']
+})
+export class PadreComponent implements OnInit {
+
+  name: string;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.name = '';
+  }
+
+}
